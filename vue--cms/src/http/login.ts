0bad6cd2bbe091b1f8cd25362a43fd41a23ac6a8
Xu-Zhilin <19175855391@163.com>
2026-06-27 18:47:37 +0800

    前端界面小改

--- vue--cms/src/http/login.ts
+++ vue--cms/src/http/login.ts
@@ -2,7 +2,7 @@ import axios from '@/http/axios.ts'
 
 export const login = (data: any) => {
    return  axios({
-        url:'/api/auth/login',
+        url:'/auth/login',
         method: 'POST',
         data:data,
     })
